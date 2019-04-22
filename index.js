754d0be706ce2eb18578f8a1e222bf0d4c9d7387
Rachel Fowler <rachelfowler17@gmail.com>
2019-04-22 19:04:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ function logShout(string) { console.log("logShout").toUpperCase() }
 
 function logWhisper(string) {console.log ("logWhisper").toUpperCase() } 
 
-function sayHiToGrandma(string) returns {console.log ("Hi, Grandma!") ;Expected("I can't hear you!") if 
+function sayHiToGrandma(string) returns {console.log ("Hi, Grandma!")} ;Expected("I can't hear you!") if 
