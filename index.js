33bb6157f57ceccc0b4186034acb38863d8b4c54
Rachel Fowler <rachelfowler17@gmail.com>
2019-04-22 18:54:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,8 @@ function whisper(string)
 {
   return string.toLowerCase()
 }
-function logShout(string) console.log("logShout").toUpperCase()
+function logShout(string) { console.log("logShout").toUpperCase() }
+
+function logWhisper(string) {console.log ("logWhisper").toUpperCase() } 
+
+function sayHiToGrandma(string) returns {console.log ("hi, grandma")} 
